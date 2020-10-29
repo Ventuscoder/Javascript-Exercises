eace5fa1473f3adebf77060c900317821622c2fd
ventuscoder <shivankcode@gmail.com>
2020-10-29 11:41:59 +0530

    Finished Javascript Exercises

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,42 @@
-function add () {
-	
+function add (...args) {
+	return args[0] + args[1];
 }
 
-function subtract () {
-	
+function subtract (...args) {
+	return args[0] + args[1];
 }
 
-function sum () {
-	
+function sum (...args) {
+	let sumOfAll = 0;
+	args.forEach(element=> {
+		sumOfAll += element;
+	});
+	return sumOfAll;
 }
 
-function multiply () {
-	
+function multiply (...args) {
+	return array.length ? array.reduce((accumulator, nextItem) => accumulator * nextItem) : 0;
 }
 
-function power() {
-	
+function power(...args) {
+	return Math.pow(args[0], args[1]);
 }
 
-function factorial() {
-	
+function factorial(num) {
+	if (num === 0) {
+		return 1;
+	} else {
+		let fact = 1;
+		for (let i = 1; i <= num; num++) {
+			fact *= i;
+		}
+	}
+	return fact
+}
+
+function recursiveFactorial() {
+	if (n===0) return 1;
+	return n * recursiveFactorial(n-1);
 }
 
 module.exports = {
--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
-}
-
-module.exports = fibonacci
+const fibonacci = function(count) {
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < count; i++) {
+      let temp = b;
+      b = a + b;
+      a = temp;
+    }
+    return b;
+  };
+  
+  module.exports = fibonacci;
+  
